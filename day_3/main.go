493277cdf321b9649d2736f6bf9a353f8895d2b5
alexpresthus <alexander.presthus@gmail.com>
2023-12-05 22:32:32 +0100

    day 3 part 2

--- day_3/main.go
+++ day_3/main.go
@@ -21,45 +21,49 @@ func isSymbol(r rune) bool {
 	return true
 }
 
+func isGear(r rune) bool {
+	return r == '*'
+}
+
 // Returns an []int of the indexes adjacent to the provided i...j indexes in a 11 x ? matrix (including line feed)
-func isSymbolAtAdjancentIndex(i int, j int, matrix *[]rune) bool {
+func someCheckAtAdjancentIndex(i int, j int, matrix *[]rune, check func(r rune) bool) (int, bool) {
 	w := 141
 	h := len(*matrix) / w
 
 	// check side and diagonal indexes if not edges
 	if i%w != 0 {
 		// left
-		if isSymbol((*matrix)[i-1]) {
-			return true
+		if check((*matrix)[i-1]) {
+			return i - 1, true
 		}
 		// left over
 		if i/w > 0 {
-			if isSymbol((*matrix)[i-1-w]) {
-				return true
+			if check((*matrix)[i-1-w]) {
+				return i - 1 - w, true
 			}
 		}
 		// left under
 		if i/w < h-1 {
-			if isSymbol((*matrix)[i-1+w]) {
-				return true
+			if check((*matrix)[i-1+w]) {
+				return i - 1 + w, true
 			}
 		}
 	}
 	if j%w != w-1 {
 		// right
-		if isSymbol((*matrix)[j+1]) {
-			return true
+		if check((*matrix)[j+1]) {
+			return j + 1, true
 		}
 		// right over
 		if j/w > 0 {
-			if isSymbol((*matrix)[j+1-w]) {
-				return true
+			if check((*matrix)[j+1-w]) {
+				return j + 1 - w, true
 			}
 		}
 		// right under
 		if j/w < h-1 {
-			if isSymbol((*matrix)[j+1+w]) {
-				return true
+			if check((*matrix)[j+1+w]) {
+				return j + 1 + w, true
 			}
 		}
 	}
@@ -68,27 +72,28 @@ func isSymbolAtAdjancentIndex(i int, j int, matrix *[]rune) bool {
 	for x := i; x <= j; x++ {
 		// over
 		if x/w > 0 {
-			if isSymbol((*matrix)[x-w]) {
-				return true
+			if check((*matrix)[x-w]) {
+				return x - w, true
 			}
 		}
 		// under
 		if x/w < h-1 {
-			if isSymbol((*matrix)[x+w]) {
-				return true
+			if check((*matrix)[x+w]) {
+				return x + w, true
 			}
 		}
 	}
 
-	return false
+	return -1, false
 }
 
 func main() {
 	content, err := os.ReadFile("/tmp/aoc/input.txt")
 	check(err)
 	runes := bytes.Runes(content)
-	var sum int = 0
+	var sum1 int = 0
 	var curNumRunes []rune = make([]rune, 0, 5)
+	gearPartsMap := make(map[int][]int) // idx: []int
 	for i, r := range runes {
 		// add digits to current num
 		if unicode.IsDigit(r) {
@@ -97,14 +102,31 @@ func main() {
 		}
 		// not a digit - check current num for adjacent symbols
 		if len(curNumRunes) > 0 {
-			if isAdjacentSymbol := isSymbolAtAdjancentIndex(i-len(curNumRunes), i-1, &runes); isAdjacentSymbol != false {
+			if _, isAdjacentSymbol := someCheckAtAdjancentIndex(i-len(curNumRunes), i-1, &runes, isSymbol); isAdjacentSymbol != false {
 				num, err := strconv.Atoi(string(curNumRunes))
 				check(err)
-				sum += num
+				sum1 += num
 			}
-
+			if gearIdx, isAdjacentGear := someCheckAtAdjancentIndex(i-len(curNumRunes), i-1, &runes, isGear); isAdjacentGear != false {
+				num, err := strconv.Atoi(string(curNumRunes))
+				check(err)
+				gearNums, ex := gearPartsMap[gearIdx]
+				if ex != true {
+					gearPartsMap[gearIdx] = []int{num}
+				} else {
+					gearPartsMap[gearIdx] = append(gearNums, num)
+				}
+			}
 		}
 		curNumRunes = curNumRunes[:0]
 	}
-	fmt.Printf("Part 1 sum: %d\n", sum)
+	var sumGearRatios int = 0
+	for _, nums := range gearPartsMap {
+		if len(nums) == 2 {
+			sumGearRatios += nums[0] * nums[1]
+		}
+	}
+
+	fmt.Printf("Part 1 sum: %d\n", sum1)
+	fmt.Printf("Part 2 sum of gear ratios %v\n", sumGearRatios)
 }
